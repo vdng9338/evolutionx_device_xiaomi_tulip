29948356423f779be519ff1fda335dc9311f7097
HolyAngel <slverwolf@gmail.com>
2018-11-27 14:31:17 -0600

    tulip: props: Set OpenGL Skia as default renderer

--- properties.mk
+++ properties.mk
@@ -138,6 +138,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=skiagl \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
     debug.sf.enable_hwc_vds=1 \
