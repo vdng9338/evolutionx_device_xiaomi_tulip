804525837302f8e9a638cf8cdbddac0a9c6624a5
OdSazib <odsazib@gmail.com>
2021-03-10 03:38:58 +0600

    tulip: Fix sensorservice

--- device.mk
+++ device.mk
@@ -382,7 +382,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0
 
 # Shims
 PRODUCT_PACKAGES += \
