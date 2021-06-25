cd79066e615938b96377314f36fff8d3f745f35c
arturo51236 <rtsanonynousperson@gmail.com>
2021-06-26 00:25:03 +0200

    tulip: Definitely fix sensors VINTF
    * My mistake doing cherry-picks incorrectly.
    Thanks to @Dybios again.
    
    This commit reverts:
     - tulip: Fix sensorservice (https://github.com/arturo51236/device_xiaomi_tulip/commit/804525837302f8e9a638cf8cdbddac0a9c6624a5)
    
    And fixes:
    - tulip: Fix android.hardware.sensors VINTF (https://github.com/arturo51236/device_xiaomi_tulip/commit/a13613477205dd5c9234661d1f92932ff33a3c6c)

--- device.mk
+++ device.mk
@@ -382,8 +382,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service \
-    android.frameworks.sensorservice@1.0
+    android.hardware.sensors@1.0-service
 
 # Shims
 PRODUCT_PACKAGES += \
