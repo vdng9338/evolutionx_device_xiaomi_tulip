a941620624635a4881fefca75d4f3de98dcee895
arturo51236 <rtsanonynousperson@gmail.com>
2021-07-13 13:37:29 +0200

    tulip: remove_packages: Update packages to remove
     - Update it to align it to https://gitlab.com/EvoX/vendor_gapps
     - Add TurboPrebuilt to the list (causes crashes in logs and it's not very important)

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,12 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2
-LOCAL_OVERRIDES_PACKAGES += ConnMO DCMO DMService GCS GoogleCamera MaestroPrebuilt Maps
-LOCAL_OVERRIDES_PACKAGES += MicropaperPrebuilt MyVerizonServices OBDM_Permissions OemDmTrigger Ornament
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap SoundAmplifierPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SprintDM SprintHM Tycho USCCDM VZWAPNLib VzwOmaTrigger WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += Youtube YouTubeMusicPrebuilt arcore obdm_stub
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Camera2 ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions Ornament SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger WallpapersBReel2020 arcore obdm_stub
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
